7973fe9c24113c24f08ea6d2ea2847eef92dd16c
flostingapplesauce <rhoombaoh@sonofabitch.org>
2025-01-04 10:24:08 -0500

    Add modularized site structure

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+{"@context":"https://schema.org","@graph":[{"@type":["Article","BlogPosting"],"@id":"/#article","isPartOf":{"@id":"/"},"author":{"name":"Harrumph","@id":"/#/schema/person/0f04cbfc65bfad4007218fe05bddfeeb"},"headline":"Front","datePublished":"2024-12-21T00:02:58+00:00","dateModified":"2024-12-27T23:25:17+00:00","mainEntityOfPage":{"@id":"/"},"wordCount":11,"publisher":{"@id":"/#/schema/person/504ad5adcc3f59c8dc5b0c0ceb069fa5"},"image":{"@id":"/#primaryimage"},"thumbnailUrl":"/wp-content/uploads/2024/12/unnamed.jpg","inLanguage":"en-US"},{"@type":"WebPage","@id":"/","url":"/","name":"Technoshamanism, 5GW, Cyberpunk, 93/93, Thelema","isPartOf":{"@id":"/#website"},"about":{"@id":"/#/schema/person/504ad5adcc3f59c8dc5b0c0ceb069fa5"},"primaryImageOfPage":{"@id":"/#primaryimage"},"image":{"@id":"/#primaryimage"},"thumbnailUrl":"/wp-content/uploads/2024/12/unnamed.jpg","datePublished":"2024-12-21T00:02:58+00:00","dateModified":"2024-12-27T23:25:17+00:00","description":"Occultism, Personal Realization","breadcrumb":{"@id":"/#breadcrumb"},"inLanguage":"en-US","potentialAction":[{"@type":"ReadAction","target":["/"]}]},{"@type":"ImageObject","inLanguage":"en-US","@id":"/#primaryimage","url":"/wp-content/uploads/2024/12/unnamed.jpg","contentUrl":"/wp-content/uploads/2024/12/unnamed.jpg","width":960,"height":360},{"@type":"BreadcrumbList","@id":"/#breadcrumb","itemListElement":[{"@type":"ListItem","position":1,"name":"Front"}]},{"@type":"WebSite","@id":"/#website","url":"/","name":"5GW Resistance - Technoshamanism - Quantum Wave Packet Personalization","description":"418","publisher":{"@id":"/#/schema/person/504ad5adcc3f59c8dc5b0c0ceb069fa5"},"alternateName":"Do what thou wilt shall be the whole of the Law","potentialAction":[{"@type":"SearchAction","target":{"@type":"EntryPoint","urlTemplate":"/?s={search_term_string}"},"query-input":{"@type":"PropertyValueSpecification","valueRequired":true,"valueName":"search_term_string"}}],"inLanguage":"en-US"},{"@type":["Person","Organization"],"@id":"/#/schema/person/504ad5adcc3f59c8dc5b0c0ceb069fa5","name":"Kalei Gawd","image":{"@type":"ImageObject","inLanguage":"en-US","@id":"/#/schema/person/image/","url":"/wp-content/uploads/2024/12/star.jpg","contentUrl":"/wp-content/uploads/2024/12/star.jpg","width":1000,"height":1000,"caption":"Kalei Gawd"},"logo":{"@id":"/#/schema/person/image/"},"description":"I love good food, music, technology, futurism, psychedelia, reading, computers, radio, telephones, cats, HA and security. Italian food, SciFi, and Current 93.","sameAs":["","https://www.facebook.com/profile.php?id=100093031806694","https://www.instagram.com/harrumpph/","https://www.pinterest.com/93Tobit93/","https://x.com/418kaleigawd418","https://93kaleigawd93.tumblr.com/","@harrumph@mastodon.social"]},{"@type":"Person","@id":"/#/schema/person/0f04cbfc65bfad4007218fe05bddfeeb","name":"Harrumph","image":{"@type":"ImageObject","inLanguage":"en-US","@id":"/#/schema/person/image/","url":"https://secure.gravatar.com/avatar/99e53bbf4ef3a8243387fdc742a5e97f?s=96&d=mm&r=g","contentUrl":"https://secure.gravatar.com/avatar/99e53bbf4ef3a8243387fdc742a5e97f?s=96&d=mm&r=g","caption":"Harrumph"},"sameAs":["","https://www.facebook.com/harrumpph","https://www.instagram.com/harrumpph/","https://www.pinterest.com/93Tobit93/","https://x.com/https://twitter.com/lastnamed18281","https://www.tumblr.com/blog/93kaleigawd93","@harrumph@mastodon.social"],"honorificPrefix":"Sire","honorificSuffix":"-asaurus","birthDate":"01-01-0001","gender":"nonr","knowsAbout":["Hungarian Thai Food"],"knowsLanguage":["Depends upon which translator software is in front of me"],"jobTitle":"Titler of Jobs","worksFor":"The Job Titling Company"}]}
+window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
+gtag("set","linker",{"domains":["virginorpigeon.org"]});
+gtag("js", new Date());
+gtag("set", "developer_id.dZTNiMT", true);
+gtag("config", "GT-5N298LGJ", {"googlesitekit_post_type":"page"});
+ window._googlesitekit = window._googlesitekit || {}; window._googlesitekit.throttledEvents = []; window._googlesitekit.gtagEvent = (name, data) => { var key = JSON.stringify( { name, data } ); if ( !! window._googlesitekit.throttledEvents[ key ] ) { return; } window._googlesitekit.throttledEvents[ key ] = true; setTimeout( () => { delete window._googlesitekit.throttledEvents[ key ]; }, 5 ); gtag( "event", name, { ...data, event_source: "site-kit" } ); }
+(function(c,l,a,r,i,t,y){
+					c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;
+					t.src="https://www.clarity.ms/tag/"+i+"?ref=wordpress";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+				})(window, document, "clarity", "script", "pkrdmznrkj");
+( function( w, d, s, l, i ) {
+				w[l] = w[l] || [];
+				w[l].push( {'gtm.start': new Date().getTime(), event: 'gtm.js'} );
+				var f = d.getElementsByTagName( s )[0],
+					j = d.createElement( s ), dl = l != 'dataLayer' ? '&l=' + l : '';
+				j.async = true;
+				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+				f.parentNode.insertBefore( j, f );
+			} )( window, document, 'script', 'dataLayer', 'GTM-KNHS7KV9' );
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1067817841001140');
+fbq('track', 'PageView');
+var wpilFrontend = {"ajaxUrl":"\/wp-admin\/admin-ajax.php","postId":"9288","postType":"post","openInternalInNewTab":"1","openExternalInNewTab":"1","disableClicks":"0","openLinksWithJS":"0","trackAllElementClicks":"0","clicksI18n":{"imageNoText":"Image in link: No Text","imageText":"Image Title: ","noText":"No Anchor Text Found"}};
+var translation = {"next":"Next","previous":"Previous","close":"Close","loading":"Loading"};
+var csco_mega_menu = {"rest_url":"\/wp-json\/csco\/v1\/menu-posts"};
+var csSchemeLocalize = {"siteSchemeMode":"dark","siteSchemeToogle":"1"};
+_stq = window._stq || [];
+_stq.push([ "view", JSON.parse("{\"v\":\"ext\",\"blog\":\"222527107\",\"post\":\"9288\",\"tz\":\"-4\",\"srv\":\"virginorpigeon.org\",\"j\":\"1:14.1\"}") ]);
+_stq.push([ "clickTrackerInit", "222527107", "9288" ]);
+(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'8fc0eab6dd156fe9',t:'MTczNTg4NTU4OS4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();
